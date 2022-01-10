e8a21c1424b6ace07a3a7e1aa9718ad06662fe2b
kannan-santharam <kannan.santharam@freshworks.com>
2022-01-10 20:32:06 +0530

    showing data dynamically (json from static file)

--- app/components/product.js
+++ app/components/product.js
@@ -3,5 +3,5 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ProductComponent extends Component {
-
+    productImage = this.args.product.colors[0].image;
 }
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
-
+import { products } from '../data/product';
 export default class ItemRoute extends Route {
   model(params) {
     const { itemId } = params;
-    return itemId;
+    const product = products.find(({ id }) => id === itemId)
+    return product;
   }
 }
